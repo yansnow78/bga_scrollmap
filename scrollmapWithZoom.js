8e036bd93bbdbeec88ab2aef5d450c19a80b6fde
yansnow78 <ygrugni@gmail.com>
2024-05-17 16:09:35 +0200

    correct enabling/disabling of increase/decrease height buttons

--- scrollmapWithZoom.js
+++ scrollmapWithZoom.js
@@ -1,5 +1,5 @@
 /*
-ScrollmapWithZoom 1.34.0: Improved version of scrollmap used in multiple bga game
+ScrollmapWithZoom 1.34.1: Improved version of scrollmap used in multiple bga game
 https://github.com/yansnow78/bga_scrollmap.git
 
 # improvements
